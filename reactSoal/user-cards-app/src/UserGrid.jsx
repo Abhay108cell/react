a8ea410a1a6acab07f2e4d71e02705707cba8b94
abhay kumar <abhaykumar2871999@gmail.com>
2024-04-14 22:08:12 +0530

    initalize repo

---
+++ reactSoal/user-cards-app/src/UserGrid.jsx
@@ -0,0 +1,15 @@
+// UserGrid.js
+import React from 'react';
+import UserCard from './UserCard';
+
+const UserGrid = ({ users }) => {
+  return (
+    <div className="user-grid">
+      {users.map((user, index) => (
+        <UserCard key={index} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default UserGrid;
