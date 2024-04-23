7f27d84cf73c3d770060af469dd223bee2b64992
abhay kumar <abhaykumar2871999@gmail.com>
2024-04-23 18:37:09 +0530

    adding button and modify css

--- src/LotteryGame.jsx
+++ src/LotteryGame.jsx
@@ -4,6 +4,11 @@ import { genNumber, sum } from "./helper";
 export default function LotteryGame(){
   let [ticket, setTicket] = useState(genNumber(3))
   let isWinning = sum(ticket) === 15
+
+  let buyTicket= ()=>{
+    setTicket(genNumber(3));
+  }
+
   return(
     <div><h1>Lottery Game </h1>
     <div className="ticket">
@@ -11,7 +16,14 @@ export default function LotteryGame(){
       <span>{ticket[1]}</span>
       <span>{ticket[2]}</span>
     </div>
-    <h3>{isWinning && "congratulations you won!"}</h3>
+    <br />
+    <div className="center-button">
+    <button   onClick={buyTicket}>Buy Ticket</button>
+    <br />
+
+    </div>
+    <h3 className="center-item">{isWinning && "congratulations you won!"}</h3>
+
     </div>
    
   )
